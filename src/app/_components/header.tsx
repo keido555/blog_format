16ddc97985dd6b0fb84596d13214f944542c4452
tmp9490 <w03optfd73@gmail.com>
2023-06-15 09:48:17 +0900

    first commit

---
+++ src/app/_components/header.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { useState } from "react";
+
+import { GetWindowSize } from "../_utils/getWindowsSize";
+import { Navigation } from "./navigation";
+
+const Header = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const { width, height } = GetWindowSize();
+
+  const handleClickToggleBtn = () => setIsOpen((prevState) => !prevState);
+
+  return (
+    <>
+      <div className="w-screen h-20 bg-white shadow-lg z-40">
+        <div className="flex justify-between max-w-7xl mx-auto text-slate-800 pt-6">
+          <h1 className="mx-10 text-2xl">PRACTICE</h1>
+          <div className="mx-10 text-2xl">
+            <button
+              type="button"
+              aria-controls="navigation"
+              aria-expanded={isOpen}
+              aria-label="naviの開閉"
+              onClick={handleClickToggleBtn}
+              className="toggleButton"
+            >
+              <span className="line-1"></span>
+              <span className="line-2"></span>
+              <span className="line-3"></span>
+            </button>
+            <Navigation open={isOpen} onClick={handleClickToggleBtn} />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Header;
---
+++ src/app/_components/navigation.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import { MouseEventHandler } from "react";
+
+type NavigationProps = {
+  open: boolean;
+  onClick: MouseEventHandler;
+};
+
+export const Navigation = (props: NavigationProps) => {
+  return (
+    <nav id="navigation" aria-hidden={!props.open} className="navigation shadow-lg">
+      <div>
+        <button
+          type="button"
+          aria-controls="navigation"
+          aria-expanded={props.open}
+          aria-label="naviの開閉"
+          onClick={props.onClick}
+          className="toggleButton"
+        >
+          <span className="line-1"></span>
+          <span className="line-2"></span>
+          <span className="line-3"></span>
+        </button>
+      </div>
+      <ul className="mt-8 text-base">
+        <li className="my-auto mt-2 hover:cursor-pointer">
+          <Link href="/">HOME</Link>
+        </li>
+        <li className="my-auto mt-2 text-sky-600 underline hover:cursor-pointer"></li>
+        <li className="my-auto mt-2 text-sky-600 underline hover:cursor-pointer"></li>
+      </ul>
+    </nav>
+  );
+};
